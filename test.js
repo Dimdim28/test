248a0ed3ff12dde3c7171ad7357cce26db47d679
Dimdim28 <wwwdimanes1@gmail.com>
2021-09-09 17:45:25 +0300

    1 commit

---
+++ test.js
@@ -0,0 +1,25 @@
+
+const readline = require('readline-sync');
+
+
+
+const sum= function(x,y){
+    return Number(x)+Number(y); 
+}
+const max= function(x,y){
+    if(x>y){
+        return x;
+    }
+    else if(x==y){
+        console.log("a=b");
+    }
+    else{
+        return y;
+    }
+}
+
+
+let a =readline.question("введите 1 число ");
+let b =readline.question("введите 2 число ");
+console.log(`sum a,b =   ${sum(a,b)}`);
+console.log(`max(19,-6)= ${max(a,b)}`);
